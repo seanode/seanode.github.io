ae683c26442ff0ef73172cf97ceb121681aaf968
Heather Moore <heather.moore@formidable.com>
2016-08-02 14:34:38 -0700

    August 31 Meet-up- First Attempt

---
+++ _posts/[WIP]2016-08-02-2016-august-2016-couchbase-for-node.js
@@ -0,0 +1,52 @@
+---
+layout: post
+title: "August 2016 Meetup - CouchBase and Node.js"
+description: ""
+tags: [node.js, javascript, couchbase]
+---
+{% include JB/setup %}
+
+The Seattle Node.js Meetup is pleased to announce our
+[meetup](TODO)
+on **Wednesday, August 31st,** CouchBase and Node.js
+
+As always, you can find all things Seattle Node.js-related at the group website:
+[http://seanode.github.io/](http://seanode.github.io/). And you can reach out to
+us via email at [{{site.author.email}}](mailto:{{site.author.email}}) or Twitter
+at [@{{site.author.twitter}}](https://twitter.com/{{ site.author.twitter }}).
+
+See you soon!
+
+## Agenda
+
+* **6:30 - 7:00pm**: Start with appetizers and mingling, courtesy of General Assembly.
+
+* **7:00pm - 8:00pm**: Listen and learn about why CouchBase is perfect for Node.js
+
+* **8:30pm - on**: Mingle, mix, chat things Node.js-related and more as we wind down.
+
+<!-- more start -->
+
+## Talks
+
+### CouchBase and Node.js
+
+**[Nic Raboy](https://github.com/nraboy)** - Developer Advocate [CouchBase](http://www.couchbase.com/)
+
+TODO: Add Description
+
+Nic is a Developer Advocate for Couchbase in the San Francisco Bay Area. He speaks 
+at developer conferences, meetups, and community events in addition to mainting a 
+blog and writing tutorials and example applications. The core of his work is making 
+sure developers and users of Couchbase are successful in whatever it is they're 
+working on. On the side, he develops mobile applications for Android and iOS. His 
+most successful mobile application is SQLTool, a database editor for Android based 
+devices. 
+
+
+## Venue / Sponsor
+
+**[General Assembly](https://generalassemb.ly/seattle)** has graciously agreed to host the event and provide
+food and tasty beverages. General Assembly is located on the third floor at 1218 3rd Avenue, Seattle, WA, 98101 
+
+<!-- more end -->
